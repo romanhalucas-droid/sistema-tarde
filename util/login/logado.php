4c3dae19764038abba04df9e9faa3b9b2f39dec0
romanhalucas-droid <romanhalucas@gmail.com>
2025-11-17 14:08:31 -0300

    2025-11-17

--- util/login/logado.php
+++ util/login/logado.php
@@ -14,3 +14,4 @@ if(!isset($_SESSION['logadosistema']) OR $_SESSION['logadosistema']==false){
 
 require_once "{$_SERVER['DOCUMENT_ROOT']}/html/sistema/dao/Funcoes.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/html/sistema/dao/UsuariosDAO.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/html/sistema/util/plugin/vendor/autoload.php";
